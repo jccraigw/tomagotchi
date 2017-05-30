c2eaa3f320dde5034f2b4485058a6c88bd587879
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-05-30 16:41:36 -0500

    working increase logic

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ var age_num = document.getElementById('age#');
 var hunger_num = document.getElementById('hunger#');
 var sleep_num = document.getElementById('sleep#');
 var bored_num = document.getElementById('bored#');
+var game_status = document.getElementById('game_status');
 
 age_num.innerHTML = pet.age;
 hunger_num.innerHTML= pet.hunger;
@@ -26,6 +27,25 @@ pet.name = document.getElementById('name').value;
 
  });
 
+var feedButton = document.getElementById('feed');
+var lightsButton = document.getElementById('lights');
+var playButton = document.getElementById('play');
+
+ var reset = function() {
+
+ 		pet.hunger = 1;
+		pet.sleepiness = 1;
+		pet.boredom = 1;
+		hunger_num.innerHTML= pet.hunger;
+	sleep_num.innerHTML = pet.sleepiness;
+	bored_num.innerHTML = pet.boredom;
+		game_status.innerHTML = "LEVEL 1";
+		setInterval(setIncrease, 2000);
+
+
+
+ }
+
  var ageIncrease = function(){
 
  	pet.age++;
@@ -35,17 +55,39 @@ pet.name = document.getElementById('name').value;
 
  setInterval(ageIncrease, 3000);
 
- var hungerIncrease = function(){
+ var setIncrease = function(){
 
  	pet.hunger++;
+ 	pet.sleepiness++;
+ 	pet.boredom++;
+
+ 	hunger_num.innerHTML= pet.hunger;
+	sleep_num.innerHTML = pet.sleepiness;
+	bored_num.innerHTML = pet.boredom;
+
+	if(pet.hunger === 10 || pet.sleepiness === 10 || boredom === 10){
+
+		game_status.innerHTML = "GAME OVER";
+
+		setInterval(reset, 2000);
+		
+		
+	}
+
+
+
+
 
  }
 
- var hungerDecrease = function(){
 
- 	pet.hunger--;
 
- }
+
+
+ setInterval(setIncrease, 4000);
+
+
+
 
 
  
